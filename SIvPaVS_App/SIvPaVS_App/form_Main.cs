da4aa791cb127f28f1db4f87f773dd1661fbfa62
arinori393 <janmar393@gmail.com>
2015-09-29 23:18:18 +0200

    Vytvorenie projektu a dokumentovej hierarchie
    Vytvoril som slozku s projektom(appkou) a uz som nahodil aj nejake veci
    do zakladneho formu:
    - pridal som Menu s nejakymi polozkami
    - pridal som do triedy zakladneho formu nejaku instruktaz k
    pomenovavaniu a nejake to rozlozenie
    
    A vytvoril som tiez jednu slozku SIvPaVS_Xml pre vsetky zalezitosti s
    XMLkom (XML schema, XSLT transformacia,...)

---
+++ SIvPaVS_App/SIvPaVS_App/form_Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIvPaVS_App
+{
+    public partial class form_Main : Form
+    {
+        #region Variables
+        // Prosim davat kazdej premennej prefix v_
+        // Priklad: v_premennaXY
+        // Este keby sme davali aj prefixi typu premennej, tak by to bolo super
+        // Priklad: v_str_stringovaPremennaXY
+
+        #endregion
+
+        #region Properties
+        // Prosim davat kazdej vlastnosti prefix p_
+        // Priklad: p_vlastnostXY
+        // + to iste co pri premennych
+        // Priklad: p_str_stringovaVlastnostXY
+
+        #endregion
+
+        #region Constructor
+
+        public form_Main()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Functions
+        // Prosim davat kazdej funkcii prefix f_
+        // Priklad: f_funkciaXY
+
+        #endregion
+
+        #region EventHandlers
+        // Prosim davat kazdemu handleru prefix eh_
+        // Priklad: eh_eventhandlerXY
+
+        #endregion
+    }
+}
